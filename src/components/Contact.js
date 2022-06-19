f330c776e33fd32c1ac0de44afb6cf845a888462
Fedy Juniansyah <fedyjuniansyah@gmail.com>
2022-06-19 19:26:42 +0700

    React App Selesai

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,19 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+import AvatarCore from "./AvatarCore";
+const Contact = (props) => {
+  let { data } = props;
+  return (
+    <div className="card">
+      <AvatarCore photo={data.photo} avatar={data.name} />
+      <div className="card-body">
+        <h3>{data.name}</h3>
+        <span>{data.phone}</span>
+        <span>{data.email}</span>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
 
+
 const Header = () => {
     return (
-        <></>
-    )
-}
+        <div className="header">
+        <h2>Call a Friend</h2>
+        <div>
+          <span>Your friendly contact app</span>
+        </div>
+      </div>
+    );
+  };
+    
 
 export default Header;
